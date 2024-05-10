b6c98e53a83381012f31743e636403453366632d
Jose Mandique <jose.mandique14@gmail.com>
2024-05-10 14:32:48 -0400

    Conectando a API de Farmancias

--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -1,6 +1,19 @@
 import axios from 'axios';
+import { useEffect } from 'react';
 
 export const MiApi = () => {
+
+    const URL = 'https://midas.minsal.cl/farmacia_v2/WS/getLocales.php'
+
+    const fetchData = async () => {
+        const {data} = await axios.get(URL)
+        const farmancias = data
+        console.log(farmancias, "farmancias")
+    }
+
+    useEffect(()=>{
+        fetchData();
+    },[])
 
     return(
         <>
