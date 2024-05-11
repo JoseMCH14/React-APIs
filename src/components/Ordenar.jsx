3fcab416868a6f402d97ac75aa00312e44b810e4
Jose Mandique <jose.mandique14@gmail.com>
2024-05-11 00:06:45 -0400

    Agregando elementos en componentes Ordenar

--- src/components/Ordenar.jsx
+++ src/components/Ordenar.jsx
@@ -1,8 +1,14 @@
+import Form from 'react-bootstrap/Form';
+
 export const Ordernar = () => {
 
     return (
         <>
-        <h2>Ordernar</h2>
+            <Form.Select aria-label="Default select example">
+                <option disabled >Ordenar por</option>
+                <option value="1">Alfabeticamente</option>
+                <option value="2">Por region</option>
+            </Form.Select>
         </>
     )
 }
